53b6309be54489ada59187f4c941ee73f4626763
Taylor Santos <taylor.p.santos@gmail.com>
2020-10-21 16:03:34 -0700

    Add if node

---
+++ include/ast/if.h
@@ -0,0 +1,32 @@
+#ifndef IF_H
+#define IF_H
+
+#include <vector>
+#include <memory>
+#include <optional>
+
+#include "statement.h"
+#include "expression.h"
+
+namespace AST {
+
+class If final : public Statement {
+private: // Fields
+    std::unique_ptr<Expression>               cond;
+    std::unique_ptr<Statement>                stmt;
+    std::optional<std::unique_ptr<Statement>> else_stmt;
+
+private: // Methods
+    void json(std::ostream &os) const override;
+
+public: // Methods
+    If(const yy::location &loc, std::unique_ptr<Expression> cond, std::unique_ptr<Statement> stmt);
+    If(const yy::location &        loc,
+       std::unique_ptr<Expression> cond,
+       std::unique_ptr<Statement>  stmt,
+       std::unique_ptr<Statement>  else_stmt);
+};
+
+} // namespace AST
+
+#endif // IF_H
