4b7a0b427b9dddced51e6f790d5b91c1e729c941
Taylor Santos <taylor.p.santos@gmail.com>
2020-11-28 01:07:29 -0800

    Add class declaration

---
+++ include/ast/class_declaration.h
@@ -0,0 +1,62 @@
+#ifndef AST_CLASS_DECLARATION_H
+#define AST_CLASS_DECLARATION_H
+
+#include "declaration.h"
+#include "type/type.h"
+
+#include <optional>
+
+namespace AST {
+
+class ClassDeclaration : public Declaration {
+public: // Aliases
+    using Ptr = std::unique_ptr<ClassDeclaration>;
+    using Vec = std::vector<Ptr>;
+
+public: // Classes
+    struct Arg {
+        std::string            name;
+        TypeChecker::Type::Ptr type;
+    };
+    struct Field {
+        std::string            name;
+        TypeChecker::Type::Ptr type;
+    };
+    struct Method {
+        std::string                           name;
+        std::vector<Arg>                      args;
+        std::optional<TypeChecker::Type::Ptr> ret_type;
+    };
+    struct Operator {
+        std::string                           operation;
+        Arg                                   arg;
+        std::optional<TypeChecker::Type::Ptr> ret_type;
+    };
+    struct Constructor {
+        std::vector<Arg> args;
+    };
+    struct Members {
+        std::vector<Field>       fields;
+        std::vector<Method>      methods;
+        std::vector<Operator>    operators;
+        std::vector<Constructor> ctors;
+    };
+
+public: // Methods
+    ClassDeclaration(
+        const yy::location &     loc,
+        std::string              name,
+        std::vector<std::string> supers,
+        Members                  members);
+
+protected: // Methods
+    void json(std::ostream &os) const override;
+
+private: // Fields
+    std::vector<std::string> supers;
+    Members                  members;
+};
+
+} // namespace AST
+
+#endif // AST_CLASS_DECLARATION_H
---
+++ src/ast/class_declaration.cpp
@@ -0,0 +1,91 @@
+#include "ast/class_declaration.h"
+
+#include <utility>
+#include "json.h"
+
+using namespace AST;
+using namespace std;
+
+ClassDeclaration::ClassDeclaration(
+    const yy::location &     loc,
+    string                   name,
+    std::vector<std::string> supers,
+    Members                  members)
+    : Declaration(loc, move(name))
+    , supers{move(supers)}
+    , members{move(members)} {}
+
+void
+ClassDeclaration::json(ostream &os) const {
+    JSON::Object obj(os);
+    obj.KeyValue("node", "class declaration");
+    obj.KeyValue("name", getVariable());
+    obj.Key("fields");
+    {
+        JSON::Array arr(os);
+        for (const auto &field : members.fields) {
+            arr.Next();
+            JSON::Object fieldObj(os);
+            fieldObj.KeyValue("name", field.name);
+            fieldObj.KeyValue("type", *field.type);
+        }
+    }
+    obj.Key("methods");
+    {
+        JSON::Array arr(os);
+        for (const auto &method : members.methods) {
+            arr.Next();
+            JSON::Object methodObj(os);
+            methodObj.KeyValue("name", method.name);
+            methodObj.Key("args");
+            {
+                JSON::Array argArr(os);
+                for (const auto &arg : method.args) {
+                    argArr.Next();
+                    JSON::Object argObj(os);
+                    argObj.KeyValue("name", arg.name);
+                    argObj.KeyValue("type", *arg.type);
+                }
+            }
+            if (method.ret_type) {
+                methodObj.KeyValue("return type", **method.ret_type);
+            }
+        }
+    }
+    obj.Key("operators");
+    {
+        JSON::Array arr(os);
+        for (const auto &op : members.operators) {
+            arr.Next();
+            JSON::Object operatorObj(os);
+            operatorObj.KeyValue("operation", op.operation);
+            operatorObj.Key("arg");
+            {
+                JSON::Object argObj(os);
+                argObj.KeyValue("name", op.arg.name);
+                argObj.KeyValue("type", *op.arg.type);
+            }
+            if (op.ret_type) {
+                operatorObj.KeyValue("return type", **op.ret_type);
+            }
+        }
+    }
+    obj.Key("constructors");
+    {
+        JSON::Array arr(os);
+        for (const auto &ctor : members.ctors) {
+            arr.Next();
+            JSON::Object ctorObj(os);
+            ctorObj.Key("args");
+            {
+                JSON::Array argArr(os);
+                for (const auto &arg : ctor.args) {
+                    argArr.Next();
+                    JSON::Object argObj(os);
+                    argObj.KeyValue("name", arg.name);
+                    argObj.KeyValue("type", *arg.type);
+                }
+            }
+        }
+    }
+}
